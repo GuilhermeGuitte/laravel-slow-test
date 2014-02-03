0e42a05b49b5428aa9fd3227dc6c0ab2ad7084e0
Guilherme Guitte <guilherme.guitte@gmail.com>
2014-02-02 23:11:40 -0200

    initial commit

---
+++ src/GuilhermeGuitte/LaravelSlowTest/RunTimeListener.php
@@ -0,0 +1,22 @@
+<?php namespace GuilhermeGuitte\LaravelSlowTest;
+
+class RunTimeListener implements PHPUnit_Framework_TestListener
+{
+    use MethodTrait;
+
+    function __construct() { }
+
+    public function endTest(PHPUnit_Framework_Test $test, $time)
+    {
+        $execution = Config::get('test.max_execution_time', 100);
+
+        if ( $time >= $execution ) {
+            $message = 'This test exceeded the run time specified. Execution time: ' .
+                $time . ' ms';
+
+            throw new PHPUnit_Framework_ExpectationFailedException(
+                $message
+            );
+        }
+    }
+}
