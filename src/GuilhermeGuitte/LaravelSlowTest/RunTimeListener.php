55f69e39128f9bb94602628b44d8070aadf93ad9
Guilherme Guitte <guilherme.guitte@gmail.com>
2014-02-02 23:34:01 -0200

    fixed namespace resolutions

--- src/GuilhermeGuitte/LaravelSlowTest/RunTimeListener.php
+++ src/GuilhermeGuitte/LaravelSlowTest/RunTimeListener.php
@@ -1,12 +1,13 @@
 <?php namespace GuilhermeGuitte\LaravelSlowTest;
+use Config;
 
-class RunTimeListener implements PHPUnit_Framework_TestListener
+class RunTimeListener implements \PHPUnit_Framework_TestListener
 {
     use MethodTrait;
 
     function __construct() { }
 
-    public function endTest(PHPUnit_Framework_Test $test, $time)
+    public function endTest(\PHPUnit_Framework_Test $test, $time)
     {
         $execution = Config::get('test.max_execution_time', 100);
 
@@ -14,7 +15,7 @@ class RunTimeListener implements PHPUnit_Framework_TestListener
             $message = 'This test exceeded the run time specified. Execution time: ' .
                 $time . ' ms';
 
-            throw new PHPUnit_Framework_ExpectationFailedException(
+            throw new \PHPUnit_Framework_ExpectationFailedException(
                 $message
             );
         }
